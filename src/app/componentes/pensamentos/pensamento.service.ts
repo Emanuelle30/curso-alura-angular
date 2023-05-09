86a46d73b331438a82a973c3486cf627c5ad5353
EcavalcaNTTDATA <emanuelle.cavalcantemessias@emeal.nttdata.com>
2023-05-08 22:06:57 -0300

    Implemantando campo de busca

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -12,13 +12,17 @@ export class PensamentoService {
   private readonly API = 'http://localhost:3000/pensamentos'
   constructor(private http: HttpClient) { }
 
-  listar(pagina: any): Observable<Pensamento[]> {
+  listar(pagina: any, filtro: string): Observable<Pensamento[]> {
 
     const itensPorPagina = '6';
 
     let params = new HttpParams()
     .set("_page", pagina)
     .set("_limit", itensPorPagina)
+
+    if (filtro.trim().length > 2) {
+      params = params.set( "q", filtro) 
+    }
     return this.http.get<Pensamento[]>(this.API, {params});
   }
 
